cf5a9d26cad8e0aa5510ebaa4538a706e0a572a3
Rustam Khan <uettaxilarmkhan2454@gmail.com>
2026-06-03 13:45:57 +0500

    Added department field

--- Student.java
+++ Student.java
@@ -3,26 +3,32 @@ public class Student {
     int studentId;
     String studentName;
     double cgpa;
+    String department;
 
-    public Student(int studentId, String studentName, double cgpa) {
+    public Student(int studentId,
+            String studentName,
+            double cgpa,
+            String department) {
         this.studentId = studentId;
         this.studentName = studentName;
         this.cgpa = cgpa;
+        this.department = department;
     }
 
     public void displayStudent() {
         System.out.println("Student ID: " + studentId);
         System.out.println("Student Name: " + studentName);
         System.out.println("CGPA: " + cgpa);
+        System.out.println("Department: " + department);
     }
 
     public static void main(String[] args) {
 
-        Student s1 = new Student(
+        Student s1 =         new Student(
                 101,
                 "Ali Khan",
-                3.75
-        );
+                        3.75,
+                        "Software Engineering");
 
         s1.displayStudent();
     }
